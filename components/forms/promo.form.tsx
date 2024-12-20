08ccc3c536f4c6593b7805e744dd83834933cf13
Abdurahmon086 <1937ali047@gmail.com>
2024-12-20 16:26:29 +0500

    feat: refactor promo form layout and update validation schema; change price and company_id types to string

--- components/forms/promo.form.tsx
+++ components/forms/promo.form.tsx
@@ -4,14 +4,13 @@ import { createPromo } from '@/actions/promo.action'
 import { promoAddSchema } from '@/lib/validation'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Send } from 'lucide-react'
-import Link from 'next/link'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
 import { Button } from '../ui/button'
 import { Checkbox } from '../ui/checkbox'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
 import { Input } from '../ui/input'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 import { Textarea } from '../ui/textarea'
@@ -28,16 +27,14 @@ function PromoForm() {
 			description_ru: '',
 			active: false,
 			code: '',
-			price: 0,
+			price: '0',
 			company_id: undefined,
-			user_id: undefined,
 		},
 	})
 
 	function onSubmit(values: z.infer<typeof promoAddSchema>) {
 		setIsLoading(true)
-		console.log('first')
-		const promise = createPromo({ ...values })
+		const promise = createPromo({ ...values, price: +values.price, company_id: +values.company_id, user_id: 1 })
 			.then(() => form.reset())
 			.finally(() => setIsLoading(false))
 
@@ -51,154 +48,131 @@ function PromoForm() {
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
-				<FormField
-					control={form.control}
-					name='title_uz'
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input disabled={isLoading} className='h-32 resize-none' placeholder={'title'} {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='title_ru'
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input disabled={isLoading} className='h-32 resize-none' placeholder={'title'} {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='code'
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input disabled={isLoading} className='h-32 resize-none' placeholder={'Code'} {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='price'
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input disabled={isLoading} className='h-32 resize-none' placeholder={'Code'} {...field} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-
-				<FormField
-					control={form.control}
-					name='active'
-					render={({ field }) => (
-						<FormItem className='flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow'>
-							<FormControl>
-								<Checkbox checked={field.value} onCheckedChange={field.onChange} />
-							</FormControl>
-							<div className='space-y-1 leading-none'>
-								<FormLabel>Active</FormLabel>
-							</div>
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='company_id'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<Select onValueChange={field.onChange} defaultValue={field.value?.toString()}>
+				<div className='grid grid-cols-2 space-x-4'>
+					<FormField
+						control={form.control}
+						name='title_uz'
+						render={({ field }) => (
+							<FormItem>
 								<FormControl>
-									<SelectTrigger>
-										<SelectValue placeholder='Select a verified email to display' />
-									</SelectTrigger>
+									<Input disabled={isLoading} className='h-14 resize-none' placeholder={'title'} {...field} />
 								</FormControl>
-								<SelectContent>
-									<SelectItem value='m@example.com'>m@example.com</SelectItem>
-									<SelectItem value='m@google.com'>m@google.com</SelectItem>
-									<SelectItem value='m@support.com'>m@support.com</SelectItem>
-								</SelectContent>
-							</Select>
-							<FormDescription>
-								You can manage email addresses in your <Link href='/examples/forms'>email settings</Link>.
-							</FormDescription>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='user_id'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<Select onValueChange={field.onChange} defaultValue={field.value?.toString()}>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='title_ru'
+						render={({ field }) => (
+							<FormItem>
 								<FormControl>
-									<SelectTrigger>
-										<SelectValue placeholder='Select a verified email to display' />
-									</SelectTrigger>
+									<Input disabled={isLoading} className='h-14 resize-none' placeholder={'title'} {...field} />
 								</FormControl>
-								<SelectContent>
-									<SelectItem value='m@example.com'>m@example.com</SelectItem>
-									<SelectItem value='m@google.com'>m@google.com</SelectItem>
-									<SelectItem value='m@support.com'>m@support.com</SelectItem>
-								</SelectContent>
-							</Select>
-							<FormDescription>
-								You can manage email addresses in your <Link href='/examples/forms'>email settings</Link>.
-							</FormDescription>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='description_uz'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>
-								Short description<span className='text-red-500'>*</span>
-							</FormLabel>
-							<FormControl>
-								<Textarea {...field} className='h-44 bg-secondary' placeholder='Description' disabled={isLoading} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='description_ru'
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>
-								Short description<span className='text-red-500'>*</span>
-							</FormLabel>
-							<FormControl>
-								<Textarea {...field} className='h-44 bg-secondary' placeholder='Description' disabled={isLoading} />
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-
-				<Button className='w-fit rounded-full' size={'lg'} type='submit' disabled={isLoading}>
-					<span>send</span>
-					<Send className='ml-2 size-4' />
-				</Button>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<div className='grid grid-cols-3 space-x-4'>
+					<FormField
+						control={form.control}
+						name='code'
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input disabled={isLoading} className='h-14 resize-none' placeholder={'Code'} {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='price'
+						render={({ field }) => (
+							<FormItem>
+								<FormControl>
+									<Input disabled={isLoading} className='h-14 resize-none' placeholder={'Code'} {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='company_id'
+						render={({ field }) => (
+							<FormItem>
+								<Select onValueChange={field.onChange} defaultValue={field.value?.toString()}>
+									<FormControl>
+										<SelectTrigger className='h-14 resize-none'>
+											<SelectValue placeholder='Select a company' />
+										</SelectTrigger>
+									</FormControl>
+									<SelectContent>
+										<SelectItem value='1'>1</SelectItem>
+										<SelectItem value='2'>2</SelectItem>
+										<SelectItem value='3'>3</SelectItem>
+									</SelectContent>
+								</Select>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<div className='grid grid-cols-2 space-x-4'>
+					<FormField
+						control={form.control}
+						name='description_uz'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>
+									Short description<span className='text-red-500'>*</span>
+								</FormLabel>
+								<FormControl>
+									<Textarea {...field} className='h-44 bg-secondary' placeholder='Description' disabled={isLoading} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='description_ru'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>
+									Short description<span className='text-red-500'>*</span>
+								</FormLabel>
+								<FormControl>
+									<Textarea {...field} className='h-44 bg-secondary' placeholder='Description' disabled={isLoading} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+				</div>
+				<div className='flex items-center space-x-4 justify-end'>
+					<FormField
+						control={form.control}
+						name='active'
+						render={({ field }) => (
+							<FormItem className='flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow'>
+								<FormControl>
+									<Checkbox checked={field.value} onCheckedChange={field.onChange} />
+								</FormControl>
+								<div className='space-y-1 leading-none'>
+									<FormLabel>Active</FormLabel>
+								</div>
+							</FormItem>
+						)}
+					/>
+					<Button className='w-fit rounded-full' size={'lg'} type='submit' disabled={isLoading}>
+						<span>send</span>
+						<Send className='ml-2 size-4' />
+					</Button>
+				</div>
 			</form>
 		</Form>
 	)
--- lib/validation.ts
+++ lib/validation.ts
@@ -5,17 +5,16 @@ export const promoAddSchema = z.object({
 	title_ru: z.string().min(3),
 	description_uz: z.string(),
 	description_ru: z.string(),
-	price: z.number(),
+	price: z.string(),
 	active: z.boolean(),
 	code: z.string().min(4),
-	company_id: z.number(),
-	user_id: z.number(),
+	company_id: z.string(),
 })
 
 export const companyAddSchema = z.object({
 	title: z.string().min(3),
 	description: z.string(),
-	image: z.string(),	
+	image: z.string(),
 	active: z.boolean(),
 	website: z.string(),
 })
